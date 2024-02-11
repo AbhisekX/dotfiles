9e8e869fdf0c6ab7e6b9afc311d45c4494dedef2
Abhisek Das <dasabhisek1999@gmail.com>
2024-02-11 22:15:14 +0530

    bug fix lsp-config.lua

--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -36,6 +36,7 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             local lsp_zero = require("lsp-zero")
+            lsp_zero.extend_lspconfig()
 
             lsp_zero.on_attach(function(client, bufnr)
                 lsp_zero.default_keymaps({ buffer = bufnr })
