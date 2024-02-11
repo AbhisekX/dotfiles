6a0904e28f0778ab32bd3f6670852b3717cf5270
Abhisek Das <dasabhisek1999@gmail.com>
2024-02-11 19:31:41 +0530

    update config of indent.lua

--- neovim/.config/nvim/lua/plugins/indent.lua
+++ neovim/.config/nvim/lua/plugins/indent.lua
@@ -2,5 +2,26 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {},
+    opts = {
+        indent = {
+            char = "│",
+            tab_char = "│",
+        },
+        scope = { enabled = false },
+        exclude = {
+            filetypes = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
+        },
+    },
 }
